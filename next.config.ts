9c2a47ed49cadbfd98bab1b294a029f61964b5cc
Naseema Rayin <naseema.rayin@gmail.com>
2026-02-04 01:36:36 +0400

    Changes to next-config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fakestoreapi.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "plus.unsplash.com",
-        pathname: "/**",
-      },
-    ],
-  },
+  domains: [
+    "fakestoreapi.com",
+    "images.unsplash.com",
+    "plus.unsplash.com"
+  ],
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "fakestoreapi.com",
+      pathname: "/**",
+    },
+    {
+      protocol: "https",
+      hostname: "images.unsplash.com",
+      pathname: "/**",
+    },
+    {
+      protocol: "https",
+      hostname: "plus.unsplash.com",
+      pathname: "/**",
+    },
+  ],
+},
 };
 
 module.exports = nextConfig;
